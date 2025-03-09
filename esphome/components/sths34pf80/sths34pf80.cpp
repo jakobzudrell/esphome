3459ae98aaf66e7616900b3b8120b2f03392fec9
Jakob Zudrell <jakob@zudrell.eu>
2025-03-09 11:20:50 +0100

    update sths34pf80

--- esphome/components/sths34pf80/sths34pf80.cpp
+++ esphome/components/sths34pf80/sths34pf80.cpp
@@ -178,15 +178,20 @@ void STHS34PF80Component::dump_config() {
   this->read_byte(STHS34PF80_REGISTER_LPF2, &data);
   ESP_LOGCONFIG(TAG, "  lpf_a_t: %d (actual: %d)", this->lpf_a_t_, (data & 0x07));
 
-  uint16_t data1;
-  this->read_bytes(STHS34PF80_REGISTER_PRESENCE_THS_L, reinterpret_cast<uint8_t *>(&data1), 2);
-  ESP_LOGCONFIG(TAG, "  presence_threshold: %d (actual: %d)", this->presence_threshold_, data1);
+  uint8_t data_low;
+  uint8_t data_high;
+  this->read_byte(STHS34PF80_REGISTER_PRESENCE_THS_L, &data_low);
+  this->read_byte(STHS34PF80_REGISTER_PRESENCE_THS_H, &data_high);
+  ESP_LOGCONFIG(TAG, "  presence_threshold: %d (actual: %d)", this->presence_threshold_, (data_high << 8) | data_low);
 
-  this->read_bytes(STHS34PF80_REGISTER_MOTION_THS_L, reinterpret_cast<uint8_t *>(&data1), 2);
-  ESP_LOGCONFIG(TAG, "  motion_threshold: %d (actual: %d)", this->motion_threshold_, data1);
+  this->read_byte(STHS34PF80_REGISTER_MOTION_THS_L, &data_low);
+  this->read_byte(STHS34PF80_REGISTER_MOTION_THS_H, &data_high);
+  ESP_LOGCONFIG(TAG, "  motion_threshold: %d (actual: %d)", this->motion_threshold_, (data_high << 8) | data_low);
 
-  this->read_bytes(STHS34PF80_REGISTER_TAMB_SHOCK_THS_L, reinterpret_cast<uint8_t *>(&data1), 2);
-  ESP_LOGCONFIG(TAG, "  tamb_shock_threshold: %d (actual: %d)", this->tamb_shock_threshold_, data1);
+  this->read_byte(STHS34PF80_REGISTER_TAMB_SHOCK_THS_L, &data_low);
+  this->read_byte(STHS34PF80_REGISTER_TAMB_SHOCK_THS_H, &data_high);
+  ESP_LOGCONFIG(TAG, "  tamb_shock_threshold: %d (actual: %d)", this->tamb_shock_threshold_,
+                (data_high << 8) | data_low);
 
   ESP_LOGCONFIG(TAG, "  presence_hysteresis: %d (actual: %d)", this->presence_hysteresis_, 0x00);
   ESP_LOGCONFIG(TAG, "  motion_hysteresis: %d (actual: %d)", this->motion_hysteresis_, 0x00);
