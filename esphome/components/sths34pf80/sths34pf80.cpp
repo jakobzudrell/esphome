fa1e312eafc48f6e4d5dce816c3cb9cd75792200
Jakob Zudrell <jakob@zudrell.eu>
2025-03-09 18:08:03 +0100

    update sths34pf80

--- esphome/components/sths34pf80/sths34pf80.cpp
+++ esphome/components/sths34pf80/sths34pf80.cpp
@@ -192,7 +192,8 @@ void STHS34PF80Component::dump_config() {
   uint8_t data;
   this->read_byte(STHS34PF80_REGISTER_SENS_DATA, &data);
   ESP_LOGCONFIG(TAG, "  Sensitivity: %d", data);
-  ESP_LOGCONFIG(TAG, "  Sensitivity: %d", (int16_t) ((int16_t) (data << 8) | 0x00));
+  ESP_LOGCONFIG(TAG, "  Sensitivity: %d", (int8_t) (data));
+  ESP_LOGCONFIG(TAG, "  Sensitivity: %d", (int8_t) (data * 16.0 + 2048));
 
   this->read_byte(STHS34PF80_REGISTER_LPF1, &data);
   ESP_LOGCONFIG(TAG, "  lpf_p_m: %d (on sensor: %d)", this->lpf_p_m_, (data & 0x38) >> 3);
